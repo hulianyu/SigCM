eed4e047f39befd554960643d8110c16818bac5b
Lianyu Hu <hly4ml@gmail.com>
2025-05-19 23:07:54 +0800

    unused

--- SigDT/binomtest.m
+++
@@ -1,7 +0,0 @@
-function pv = binomtest(pvs,alpha)
-k = length(pvs);
-% compute test statistic
-statistic = sum(pvs <= alpha);
-% pv = sum(binopdf(statistic:k,k,alpha));
-pv = sum(simplifiedBinopdf(statistic:k,k,alpha)); 
-end
